d09e57bc4640003cf5c3aedbdeb3bed86404d35f
vagrant <vagrant@homestead>
2020-06-24 10:45:41 +0000

    基础布局1

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,8 @@
+<footer class="footer">
+    <div class="container">
+        <p>
+            由 <a href="">JackeyPeng</a> 设计和编码 <span style="color: #e27575;font-size: 14px;">💗</span>
+        </p>
+        <p class="float-right"><a href="">联系我们</a></p>
+    </div>
+</footer>
