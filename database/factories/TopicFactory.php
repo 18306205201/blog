48226e999cb69d5584cd054d8231b690f7d3f5c7
Peng Tao <pengtao@hudunsoft.com>
2020-10-30 09:37:50 +0800

    填充用户和话题数据

--- database/factories/TopicFactory.php
+++ database/factories/TopicFactory.php
@@ -1,9 +1,24 @@
 <?php
 
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Models\Topic;
 use Faker\Generator as Faker;
+use Illuminate\Support\Str;
 
 $factory->define(App\Models\Topic::class, function (Faker $faker) {
+    $sentence = $faker->sentence();
+    //随机取一个月内的时间
+    $updated = $faker->dateTimeThisMonth();
+    //创建时间永远要比创建时间要大
+    $created = $faker->dateTimeThisMonth($updated);
     return [
-        // 'name' => $faker->name,
+        'title' => $faker->title,
+        'body' => $faker->text(),
+        'excerpt' => $sentence,
+        'user_id' => $faker->randomElement([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]),
+        'category_id' => $faker->randomElement([1, 2, 3, 4]),
+        'created_at' => $created,
+        'updated_at' => $updated
     ];
 });
