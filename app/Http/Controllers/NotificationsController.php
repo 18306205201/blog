6cb2c0833ae6df33892b09d5a5d3d20c2112c59d
Peng Tao <pengtao@hudunsoft.com>
2020-11-03 17:52:46 +0800

    master 111

---
+++ app/Http/Controllers/NotificationsController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Auth;
+
+class NotificationsController extends Controller
+{
+
+    /**
+     * NotificationsController constructor.
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function index()
+    {
+        // 获取登陆用户的所有通知
+        $notifications = Auth::user()->notifications()->paginate(20);
+        Auth::user()->markAsRead();
+        return view('notifications.index', compact('notifications'));
+    }
+}
