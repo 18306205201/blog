88fc94d16bc1ea95948925b745be8f14255a3773
Peng Tao <pengtao@hudunsoft.com>
2020-11-03 17:12:09 +0800

    消息通知列表

--- routes/web.php
+++ routes/web.php
@@ -27,3 +27,4 @@ Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
 Route::post('upload_image', 'TopicsController@upload_image')->name('topics.upload_image');
 
 Route::resource('replies', 'RepliesController', ['only' => ['store', 'destroy']]);
+Route::resource('notifications', 'NotificationsController', ['only' => ['index']]);
