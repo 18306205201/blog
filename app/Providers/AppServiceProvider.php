4cbfdd459a70d2ed9e90bb8cd2d2c882653b6e23
Peng Tao <pengtao@hudunsoft.com>
2020-11-04 18:23:34 +0800

    用户注册添加默认头像

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,9 +5,11 @@ namespace App\Providers;
 use App\Models\Link;
 use App\Models\Reply;
 use App\Models\Topic;
+use App\Models\User;
 use App\Observers\LinkObserver;
 use App\Observers\ReplyObserver;
 use App\Observers\TopicObserver;
+use App\Observers\UserObserver;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
 
@@ -37,6 +39,7 @@ class AppServiceProvider extends ServiceProvider
             return \Auth::user()->hasRole('Founder');
         });
         Schema::defaultStringLength(191);
+        User::observe(UserObserver::class);
         Topic::observe(TopicObserver::class);
         Reply::observe(ReplyObserver::class);
         Link::observe(LinkObserver::class);
