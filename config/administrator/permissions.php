ae69173f9b83c965bf3db7a68a3563686a11811d
Peng Tao <pengtao@hudunsoft.com>
2020-11-04 14:21:19 +0800

    管理员可以管理角色和权限

---
+++ config/administrator/permissions.php
@@ -0,0 +1,62 @@
+<?php
+use Spatie\Permission\Models\Permission;
+
+return [
+    'title' => '权限',
+    'single' => '权限',
+    'model' => Permission::class,
+
+    'permission' => function()
+    {
+        return Auth::user()->can('manage_contents');
+    },
+
+    // 对 CURD 操作单独做权限控制，通过返回布尔值来控制权限
+    'action_permissions' => [
+        'create' => function($model) {
+            return true;
+        },
+        'update' => function($model) {
+            return true;
+        },
+        'delete' => function($model) {
+            return true;
+        },
+        'view' => function($model) {
+            return true;
+        },
+    ],
+
+    'columns' => [
+        'id' => [
+            'title' => 'ID',
+        ],
+        'name' => [
+            'title' => '标识'
+        ],
+        'operation' => [
+            'title' => '管理',
+            'sortable' => false,
+        ],
+    ],
+
+    'edit_fields' => [
+        'name' => [
+            'title' => '标示（请慎重修改）',
+            // 表单条目标题旁的『提示信息』
+            'hint' => '修改权限标识会影响代码的调用，请不要轻易更改。'
+        ],
+        'roles' => [
+            'type' => 'relationship',
+            'title' => '角色',
+            'name_field' => 'name',
+        ],
+    ],
+
+    'filters' => [
+        'name' => [
+            'title' => '标示',
+        ],
+    ],
+];
+
