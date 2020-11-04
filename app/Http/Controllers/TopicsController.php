525963c8ad0e0075ad1e67c3484c2d57a1fe33f0
Peng Tao <pengtao@hudunsoft.com>
2020-11-04 16:38:21 +0800

    活跃用户

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Category;
 use App\Models\Topic;
+use App\Models\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\TopicRequest;
@@ -17,12 +18,13 @@ class TopicsController extends Controller
         $this->middleware('auth', ['except' => ['index', 'show']]);
     }
 
-	public function index(Request $request)
+	public function index(Request $request, Topic $topic, User $user)
 	{
 		$topics = Topic::with(['category', 'user'])
             ->withOrder($request->order)
-            ->paginate(30);
-		return view('topics.index', compact('topics'));
+            ->paginate(20);
+		$active_users = $user->getActiveUsers();
+		return view('topics.index', compact('topics', 'active_users'));
 	}
 
     public function show(Request $request, Topic $topic)
